bb817ca2e1ad1324a7e78a475ef5a4806ac60ac4
下田直道 <naomichi@shimodanaomichinoMacBook-Air.local>
2020-06-07 14:48:34 +0900

    [FInish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,6 +12,8 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+
   end
 
   def create
@@ -21,6 +23,12 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog.id)
   end
 
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+
+    redirect_to blog_path(blog)
+  end
 
 private
 
